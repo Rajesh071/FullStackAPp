bbd7fea8d8607b13b279ff078416ec6d70f1b512
Rajesh Sahani <rajeshsahani@Rajeshs-MacBook-Pro.local>
2019-01-10 16:22:39 +0530

    proxy = false in passport.js for prod in heroku

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL: "/auth/google/callback",
-    proxy:true
+    proxy:false
 },async (accessToken, refreshToken, profile, done)=>{
 
     const existingUser =  await User.findOne({googleId:profile.id});
